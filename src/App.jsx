cf792fdd43a55461d6e90bfe6d7a6066da2b2e3d
makekop <72009584+makekop@users.noreply.github.com>
2025-08-12 21:25:58 +0300

    updated the code with more states

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,53 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-    const [num1, setNum1] = useState("");
-    const [num2, setNum2] = useState("");
-    const [operator, setOperator] = useState(null);
-    const [result, setResult] = useState(null);
+    const [list1, setList1] = useState([]);
+    const [list2, setList2] = useState([]);
+    const [sum, setSum] = useState(null);
+    const [activeList, setActiveList] = useState(1);
 
     const pressNum = (digit) => {
-        if (operator === null) {
-            setNum1((prev) => prev + digit);
+        if (activeList === 1) {
+            setList1((prev) => [...prev, digit]);
         } else {
-            setNum2((prev) => prev + digit);
+            setList2((prev) => [...prev, digit]);
         }
     };
 
-    const chooseOperator = (op) => {
-        setOperator(op);
+    const calculateSum = () => {
+        const num1 = Number(list1.join(""));
+        const num2 = Number(list2.join(""));
+        setSum(num1 + num2);
     };
-
-    const calculate = () => {
-        const a = Number(num1);
-        const b = Number(num2);
-        let res = null;
-
-        if (operator === "+") res = a + b;
-
-        setResult(res);
+    const clearAll = () => {
+        setList1([]);
+        setList2([]);
+        setSum(null);
     };
-
     return (
-        <>
-            <h1>
-                {" "}
-                {num1} {operator} {num2}
-            </h1>
+        <div>
+            <h2>
+                Num1: {list1.join("") || 0}
+                {"  "}
+                Num2: {list2.join("") || 0}
+            </h2>
             <div>
-                <button onClick={() => pressNum("1")}>1</button>
-                <button onClick={() => pressNum("2")}>2</button>
+                <button onClick={() => setActiveList(1)}>num1</button>
+                <button onClick={() => setActiveList(2)}>num2</button>
             </div>
             <div>
-                <button onClick={() => chooseOperator("+")}>+</button>
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((n) => (
+                    <button key={n} onClick={() => pressNum(n.toString())}>
+                        {n}
+                    </button>
+                ))}
             </div>
             <div>
-                <button onClick={() => calculate()}>=</button>
+                <button onClick={calculateSum}>+</button>
+                <button onClick={clearAll}>C</button>
+                <h1>{sum}</h1>
             </div>
-            <div>{result !== null && <h1>Result: {result}</h1>}</div>
-        </>
+        </div>
     );
 }
 
